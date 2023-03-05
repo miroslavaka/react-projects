42d75aa9f987739b35f05ad8f89cfdc3b5e3c06a
miroslavaka <kacmarova.miroslava@gmail.com>
2023-03-05 22:01:28 +0100

    map data, adding title, info, setting plus/minu buttons

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <SingleQuestion questions={data} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ questions }) => {
+  //console.log(questions);
+  const [id, info, title] = questions;
+  console.log(id, info, title);
+
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <section className="container">
+      {questions.map((question) => {
+        return (
+          <article className="question">
+            <header>
+              <h4>{question.title}</h4>
+              <button
+                className="btn"
+                onClick={() => {
+                  setIsActive(!isActive);
+                }}
+              >
+                {isActive ? <AiOutlinePlus /> : <AiOutlineMinus />}
+              </button>
+            </header>
+            {isActive && <p>{question.info}</p>}
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Question;
